1e94979a4d8190f70c77a44137d9b8ed6d812225
Bonnene <xivetvc@hotmail.com>
2015-12-17 11:46:31 +0100

    Added sliders

--- SlamJam/Assets/Scripts/Controls/KeyboardControls.cs
+++ SlamJam/Assets/Scripts/Controls/KeyboardControls.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class KeyboardControls : MonoBehaviour {
 
@@ -8,15 +10,18 @@ public class KeyboardControls : MonoBehaviour {
 	private KeyCode[] downCodes;
 	private Wave wave;
 	private const float rateOfChange = 0.05F;
+	private List<GameObject> UIsliders;
 
 	// Use this for initialization
 	void Start () {
+		UIsliders = new List<GameObject>();
 		upCodes = new KeyCode[] { KeyCode.Q, KeyCode.W, KeyCode.E, KeyCode.R };
 		downCodes = new KeyCode[] { KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.F };
 		wave = gameObject.GetComponent<Wave> ();
 		sliders = new float[4];
 		for (int i = 0; i < 4; ++i) {
 			sliders [i] = 0.0F;
+			UIsliders.Add(GameObject.FindGameObjectWithTag ("Slider"+i)) ;
 		}
 	}
 	
@@ -26,10 +31,13 @@ public class KeyboardControls : MonoBehaviour {
 			if (Input.GetKey (upCodes[i]) && sliders[i] < 1.0F) {
 				sliders [i] += rateOfChange;
 				wave.setVolume (i, sliders [i]);
+				UIsliders [i].GetComponent<Slider>().normalizedValue = UIsliders[i].GetComponent<Slider>().normalizedValue+rateOfChange;
 			}	
 			if (Input.GetKey (downCodes[i]) && sliders[i] > 0.0F) {
 				sliders [i] -= rateOfChange;
 				wave.setVolume (i, sliders [i]);
+				UIsliders [i].GetComponent<Slider>().normalizedValue = UIsliders[i].GetComponent<Slider>().normalizedValue-rateOfChange;
+
 			}
 		}
 	}
