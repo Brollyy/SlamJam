6d61f0f266f41bc41fa87f3da69a97024baccbec
Brollyy <neon349@gmail.com>
2015-12-17 18:54:45 +0100

    Demo version
    First demo version for lo-fi prototype.
    Added changing levels in-game.

---
+++ SlamJam/Assets/Scripts/Utility/LevelChanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelChanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			if(SceneManager.GetActiveScene().name != "level1") SceneManager.LoadScene ("level1");
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			if(SceneManager.GetActiveScene().name != "modeTest") SceneManager.LoadScene ("modeTest");
+		}
+	}
+}
