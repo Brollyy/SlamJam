82b9499a7b1e91b7258f8654f70689be95369683
Brollyy <neon349@gmail.com>
2016-01-04 15:30:50 +0100

    Setting up for gameplay prototype
    - generalizing LevelChanger
    - adding LevelChanger to all maps

--- SlamJam/Assets/Scripts/Utility/LevelChanger.cs
+++ SlamJam/Assets/Scripts/Utility/LevelChanger.cs
@@ -4,19 +4,24 @@ using System.Collections;
 
 public class LevelChanger : MonoBehaviour {
 
+	public KeyCode[] keys = { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5 };
+	private Scene[] maps;
+	private int n;
+
 	// Use this for initialization
 	void Start () {
-	
+		maps = SceneManager.GetAllScenes ();
+		n = Mathf.Min (keys.Length, maps.Length);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Alpha1)) {
-			if(SceneManager.GetActiveScene().name != "level1") SceneManager.LoadScene ("level1");
+
+		for (int i = 0; i < n; ++i) {
+			if (Input.GetKeyDown (keys[i])) {
+				if(SceneManager.GetActiveScene().name != maps[i].name) SceneManager.LoadScene (maps[i].name);
+			}
 		}
 
-		if (Input.GetKeyDown (KeyCode.Alpha2)) {
-			if(SceneManager.GetActiveScene().name != "modeTest") SceneManager.LoadScene ("modeTest");
-		}
 	}
 }
