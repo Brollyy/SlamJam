c2f15deb4dee22631bf859404a9f87feb5a65f26
Brollyy <neon349@gmail.com>
2016-01-12 14:09:28 +0100

    First iteration
    Camera bloom
    Ball trail
    Flow control during respawning

--- SlamJam/Assets/Plugins/Respawner.cs
+++ SlamJam/Assets/Plugins/Respawner.cs
@@ -4,26 +4,53 @@ using System.Collections;
 public class Respawner : MonoBehaviour {
 
 	public KeyCode respawnButton = KeyCode.Space;
+	public float respawnTime = 1.0F;
 	private GameObject timer;
+	private Transform player;
+	private Animator anim;
+	private Vector3 pos = Vector3.zero;
+	private Vector3 smoothPos = Vector3.zero;
+	private int state = 2;
 
 	// Use this for initialization
 	void Start () {
 		timer = GameObject.FindGameObjectWithTag ("Timer");
+		player = gameObject.GetComponentsInChildren<Transform> () [1];
+		anim = player.GetChild (0).gameObject.GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (respawnButton)) {
+		if (state != 1 && Input.GetKeyDown (respawnButton)) {
 			Respawn ();
 		}
 	}
 
+	void FixedUpdate() {
+
+		if (state == 1) {
+			pos = Vector3.SmoothDamp (pos, Vector3.zero, ref smoothPos, respawnTime);
+			player.localPosition = pos;
+
+			if (pos.magnitude < 0.01F) {
+				player.localEulerAngles = Vector3.zero;
+				anim.SetBool ("Active", true);
+				state = 2;
+			}
+		}
+	}
+
 	public void Respawn() {
-		timer.GetComponent<Timer> ().Restart ();
-		Transform child = gameObject.GetComponentsInChildren<Transform>()[1];
-		child.localPosition = Vector3.zero;
-		child.localEulerAngles = Vector3.zero;
-		child.gameObject.GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
-		child.gameObject.GetComponent<Rigidbody2D> ().angularVelocity = 0.0F;
+		if (state == 0) {
+			timer.GetComponent<Timer> ().Restart ();
+			pos = player.localPosition;
+			player.gameObject.GetComponent<Rigidbody2D> ().isKinematic = true;
+			anim.SetBool ("Active", false);
+			state = 1;
+		} 
+		else if (state == 2) {
+			player.gameObject.GetComponent<Rigidbody2D> ().isKinematic = false;
+			state = 0;
+		}
 	}
 }
