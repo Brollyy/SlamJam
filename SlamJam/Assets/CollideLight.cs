7c2912789e49cbefaf010a4acc39cfc08796f7b7
Brollyy <neon349@gmail.com>
2016-01-13 23:25:02 +0100

    Sound effects for boosters

--- SlamJam/Assets/CollideLight.cs
+++ SlamJam/Assets/CollideLight.cs
@@ -1,25 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CollideLight : MonoBehaviour {
 	float nextFlash = 0.0F;
 	float flashRate = 0.5F;
-	private MeshRenderer[] platforms;
+	private List<MeshRenderer> platforms;
 	public Color[] colors;
 	Color[] baseColor;
 	private bool flashing = false;
 
 	// Use this for initialization
 	void Start () {
-		platforms = gameObject.GetComponentsInChildren<MeshRenderer> ();
-		baseColor = new Color[platforms.Length];
-		for (int i = 0; i < platforms.Length; i++) {
+		Transform[] t = transform.GetComponentsInChildren<Transform> ();
+		platforms = new List<MeshRenderer> ();
+		foreach (Transform tran in t) {
+			if (tran.parent == transform) {
+				MeshRenderer mr = tran.GetComponent<MeshRenderer> ();
+				if (mr) platforms.Add (mr);
+			}
+		}
+		baseColor = new Color[platforms.Count];
+		for (int i = 0; i < platforms.Count; i++) {
 			baseColor[i] = platforms [i].material.color;
 		}
 	}
 
 	public void OnTriggerStay2D(Collider2D other){
-		if (other.transform.gameObject.name == "Player") {
+		if (other.transform.gameObject.name == "Player" &&
+			!other.transform.GetComponent<Rigidbody2D>().isKinematic) {
 			flashing = true;
 		}
 	}
@@ -41,13 +50,13 @@ public class CollideLight : MonoBehaviour {
 			//	j = Random.Range (0, colors.Length - 1);
 			//} while(j == i);
 			}
-			for (int i = 0; i < platforms.Length; i++) {
+			for (int i = 0; i < platforms.Count; i++) {
 				platforms [i].material.color = Color.LerpUnclamped (colors [0], colors [1], nextFlash / flashRate);
 				platforms [i].material.color = Color.LerpUnclamped (colors [1], colors [0], nextFlash / flashRate);
 			}
 		}
 		else {
-			for (int i = 0; i < platforms.Length; i++) {
+			for (int i = 0; i < platforms.Count; i++) {
 				platforms[i].material.color = baseColor[i];
 			}
 		}
