710f225f5f98a1c7efdd38665d29394b2d86e5b2
Brollyy <neon349@gmail.com>
2016-01-14 17:44:51 +0100

    Controls changes
    Escape quits the game and keyboard controls are no longer depending on
    framerate

--- SlamJam/Assets/Scripts/Controls/TrackKeyboardControls.cs
+++ SlamJam/Assets/Scripts/Controls/TrackKeyboardControls.cs
@@ -9,7 +9,7 @@ public class TrackKeyboardControls : MonoBehaviour {
 	public KeyCode[] upCodes = new KeyCode[] { KeyCode.Q, KeyCode.W, KeyCode.E, KeyCode.R };
 	public KeyCode[] downCodes = new KeyCode[] { KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.F };
 	private GameObject tracklist;
-	public float rateOfChange = 0.05F;
+	public float rateOfChange = 1.0F;
 	private AudioSource[] sources;
 	private int n;
 
@@ -32,14 +32,18 @@ public class TrackKeyboardControls : MonoBehaviour {
 		for (int i = 0; i < n; ++i) {
 			if (sources [i].clip) {
 				if (Input.GetKey (upCodes [i]) && sliders [i] < 1.0F) {
-					sliders [i] += rateOfChange;
+					sliders [i] += rateOfChange*Time.deltaTime;
 					sources [i].volume = sliders [i];
 				}	
 				if (Input.GetKey (downCodes [i]) && sliders [i] >= 0.0F) {
-					sliders [i] -= rateOfChange;
+					sliders [i] -= rateOfChange*Time.deltaTime;
 					sources [i].volume = (sliders [i] >= 0.0F ? sliders [i] : 0.0F);
 				}
 			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.Quit ();
 		}
 	}
 }
