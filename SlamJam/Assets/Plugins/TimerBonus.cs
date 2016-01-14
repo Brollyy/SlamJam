1411405ef824f63a4520236b4ba09fb76834bd40
Brollyy <neon349@gmail.com>
2016-01-14 15:49:49 +0100

    Made collectible text clearer and animated

--- SlamJam/Assets/Plugins/TimerBonus.cs
+++ SlamJam/Assets/Plugins/TimerBonus.cs
@@ -24,7 +24,8 @@ public class TimerBonus : MonoBehaviour {
 	void FixedUpdate () {
 		if (active) {
 			time += Time.fixedDeltaTime;
-			(text as RectTransform).anchoredPosition = Vector2.Lerp(start, end, time/animationTime);
+			(text as RectTransform).position = Vector2.Lerp(start, end, time/animationTime);
+			(text as RectTransform).localScale = Vector2.Lerp (new Vector2 (1.0F, 1.0F), Vector2.zero, time / animationTime);
 			if (time >= animationTime) {
 				active = false;
 				if (text) Destroy (text.gameObject);
@@ -41,8 +42,18 @@ public class TimerBonus : MonoBehaviour {
 				text.SetParent(canvas.transform, false);
 			}
 			text.GetComponent<Text> ().text = string.Format("{0:F}", bonus);
-			start = (text as RectTransform).anchoredPosition;
-			end = new Vector3 (start.x, start.y - 10);
+			start = (text as RectTransform).position;
+			RectTransform rect = (text as RectTransform);
+			Vector2 pivot = rect.pivot;
+			rect.pivot = new Vector2 (1.0F, 1.0F);
+			Vector2 anchorMax = rect.anchorMax;
+			rect.anchorMax = new Vector2 (1.0F, 1.0F);
+			Vector2 anchorMin = rect.anchorMin;
+			rect.anchorMin = new Vector2 (1.0F, 1.0F);
+			end = rect.position;
+			rect.pivot = pivot;
+			rect.anchorMax = anchorMax;
+			rect.anchorMin = anchorMin;
 		}
 	}
 }
