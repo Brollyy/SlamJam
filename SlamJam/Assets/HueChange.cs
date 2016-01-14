1f0a3a7789a847a451a539f498301b5a638f4c7a
Bonnene <xivetvc@hotmail.com>
2016-01-14 11:31:20 +0100

    Imported assets, graphics stuff

--- SlamJam/Assets/HueChange.cs
+++ SlamJam/Assets/HueChange.cs
@@ -8,39 +8,55 @@ public class HueChange : MonoBehaviour {
 	int i = 0;
 	public float delay = 0.0F;
 	public Color[] colors;
+	private bool rave = false;
 	
 	// Use this for initialization
 	void Start () {
+		rave = false;
+		for (int b = 0; b < backgrounds.Length; b++) {
+			backgrounds [b].GetComponentInChildren<SpriteRenderer> ().color = Color.LerpUnclamped (colors [i], Color.white, nextFlash / flashRate);
+		}
+		
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (Time.fixedTime < delay)
-			return;
-
-		if (nextFlash < flashRate) {
-			nextFlash += Time.fixedDeltaTime;
-
-		} 
-		else {
-			nextFlash = 0;
-			i++;
-			if (i == colors.Length) {
-				i = 0;
-			}
-			//i = j;
-			//do {
-			//	j = Random.Range (0, colors.Length - 1);
-			//} while(j == i);
+		if (Input.GetKeyDown ("1")) {
+			rave = !rave;
 		}
-		for (int b = 0; b < backgrounds.Length; b++) {	
-			if (i < colors.Length-1) {
-				backgrounds [b].GetComponent<SpriteRenderer> ().color = Color.LerpUnclamped (colors[i], colors [i+1], nextFlash / flashRate);
 
-			}else{
-				backgrounds [b].GetComponent<SpriteRenderer> ().color = Color.LerpUnclamped (colors[i], colors [0], nextFlash / flashRate);
+			if (Time.fixedTime < delay)
+				return;
+
+			if (nextFlash < flashRate) {
+				nextFlash += Time.fixedDeltaTime;
+
+			} else {
+				nextFlash = 0;
+				i++;
+				if (i == colors.Length) {
+					i = 0;
+				}
+				//i = j;
+				//do {
+				//	j = Random.Range (0, colors.Length - 1);
+				//} while(j == i);
+			}
+		if (rave) {
+			for (int b = 0; b < backgrounds.Length; b++) {	
+				if (i < colors.Length - 1) {
+					backgrounds [b].GetComponentInChildren<SpriteRenderer> ().color = Color.LerpUnclamped (colors [i], colors [i + 1], nextFlash / flashRate);
+
+				} else {
+					backgrounds [b].GetComponentInChildren<SpriteRenderer> ().color = Color.LerpUnclamped (colors [i], colors [0], nextFlash / flashRate);
 
 
+				}
+			}
+		} 
+		if(!rave){
+			for (int b = 0; b < backgrounds.Length; b++) {
+				backgrounds [b].GetComponentInChildren<SpriteRenderer> ().color = Color.white;
 			}
 		}
 	}
